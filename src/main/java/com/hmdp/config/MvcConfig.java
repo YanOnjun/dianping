c160980b4ffba517cfbd125b301a9a6b3de8721d
YanOnjun <2296771981@qq.com>
2023-07-20 15:38:54 +0800

    用户拦截修改

--- src/main/java/com/hmdp/config/MvcConfig.java
+++ src/main/java/com/hmdp/config/MvcConfig.java
@@ -1,8 +1,22 @@
 package com.hmdp.config;
 
+import cn.hutool.core.util.StrUtil;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.utils.LoginInterceptor;
+import com.hmdp.utils.UserHolder;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.common.RedisConstants.LOGIN_USER_KEY;
+import static com.hmdp.common.RedisConstants.LOGIN_USER_TTL;
 
 /**
  * @author : 上春
@@ -10,9 +24,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 public class MvcConfig implements WebMvcConfigurer {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoginInterceptor())
+        registry.addInterceptor(new LoginInterceptor(stringRedisTemplate))
                 .excludePathPatterns(
                         "/shop/**",
                         "/voucher/*",
@@ -22,5 +39,26 @@ public class MvcConfig implements WebMvcConfigurer {
                         "/user/code",
                         "/user/login"
                 );
+        // 拦截所有路径 用户如果登录了 更新用户过期时间
+        registry.addInterceptor(new HandlerInterceptor() {
+            @Override
+            public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+                // 获取session中的token
+                String token = request.getHeader("authorization");
+                if (StrUtil.isNotBlank(token)) {
+                    UserDTO user = UserHolder.getUser();
+                    // 去redis中查询用户数据是否存在
+                    String key = LOGIN_USER_KEY + token;
+                    Boolean hasKey = stringRedisTemplate.hasKey(key);
+                    // 如果redis中有数据
+                    if (Boolean.TRUE.equals(hasKey) && !Objects.isNull(user)) {
+                        // 更新过期时间
+                        stringRedisTemplate.expire(key, LOGIN_USER_TTL, TimeUnit.MINUTES);
+                    }
+                }
+                // 这个拦截器不会进行拦截
+                return true;
+            }
+        }).addPathPatterns("/**");
     }
 }
--- src/main/java/com/hmdp/utils/LoginInterceptor.java
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -1,13 +1,15 @@
 package com.hmdp.utils;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.StrUtil;
+import com.hmdp.common.RedisConstants;
 import com.hmdp.dto.UserDTO;
-import com.hmdp.entity.User;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.util.Objects;
+import java.util.Map;
 
 /**
  * @author : 上春
@@ -15,19 +17,32 @@ import java.util.Objects;
  */
 public class LoginInterceptor implements HandlerInterceptor {
 
+    private StringRedisTemplate redisTemplate;
+
+    public LoginInterceptor(StringRedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        // 获取session中的用户
-        HttpSession session = request.getSession();
-        Object user = session.getAttribute("user");
-        // 没有用户拦截
-        if (Objects.isNull(user)) {
-            // 重定向
-            // response.sendRedirect("/login.html");
+        // 获取session中的token
+        String token = request.getHeader("authorization");
+        if (StrUtil.isBlank(token)) {
+            response.setStatus(401);
             return false;
         }
-        // 有用户 存在 treadLocal 中
-        UserHolder.saveUser((UserDTO) user);
+        // 根据token获取redis中的用户
+        String key = RedisConstants.LOGIN_USER_KEY + token;
+        Map<Object, Object> entries = redisTemplate.opsForHash().entries(key);
+        // 没有用户拦截
+        if (entries.isEmpty()) {
+            // 重定向
+            response.setStatus(401);
+            return false;
+        }
+        // 有用户 将用户转成UserDTO 存在 treadLocal 中
+        UserDTO userDTO = BeanUtil.fillBeanWithMap(entries, new UserDTO(), false);
+        UserHolder.saveUser(userDTO);
         // 放行
         return true;
     }
