b9b2359ad0588065b2210a00095f253b6c0af8eb
YanOnjun <2296771981@qq.com>
2023-08-07 13:16:15 +0800

    添加UUID判断值是否正确 解决锁误删问题 TODO unlock方法非原子 仍然存在问题

--- src/main/java/com/hmdp/utils/reiis/RedisLock.java
+++ src/main/java/com/hmdp/utils/reiis/RedisLock.java
@@ -1,6 +1,9 @@
 package com.hmdp.utils.reiis;
 
+import cn.hutool.core.lang.UUID;
 import cn.hutool.core.util.BooleanUtil;
+import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.util.concurrent.TimeUnit;
@@ -15,6 +18,8 @@ public class RedisLock implements ILock{
 
     private final StringRedisTemplate stringRedisTemplate;
 
+    private static final String ID_PREFIX = UUID.randomUUID().toString(true);
+
     public RedisLock(String key, StringRedisTemplate stringRedisTemplate) {
         this.key = key;
         this.stringRedisTemplate = stringRedisTemplate;
@@ -22,9 +27,7 @@ public class RedisLock implements ILock{
 
     @Override
     public boolean tryLock(long timeout) {
-        String threadId = String.valueOf(Thread.currentThread().getId());
-        // since 19
-        //String threadId = String.valueOf(Thread.currentThread().threadId());
+        String threadId = getThreadId();
         // 互斥设置值
         Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(key, threadId, timeout, TimeUnit.SECONDS);
         // 存在直接返回
@@ -33,7 +36,19 @@ public class RedisLock implements ILock{
 
     @Override
     public void unlock() {
-        // 从 redis 中获取数据
-        stringRedisTemplate.delete(key);
+        String threadId = getThreadId();
+        // TODO 下面两步非原子
+        String s = stringRedisTemplate.opsForValue().get(key);
+        if (StrUtil.equals(threadId, s)){
+            // 从 redis 中获取数据
+            stringRedisTemplate.delete(key);
+        }
+        // 不是当前线程的锁 可能已经超时了
+    }
+
+    private String getThreadId() {
+        // since 19
+        //return String.valueOf(Thread.currentThread().threadId());
+        return ID_PREFIX + Thread.currentThread().getId();
     }
 }
